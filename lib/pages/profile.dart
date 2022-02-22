b628d6865b88a117a3d89be52a5ea1705c8f8801
AnawaratneMANA <it19086408@my.sliit.lk>
2022-02-22 16:03:22 +0530

    Icon added and border radius set to the image.

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -7,49 +7,62 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title:  Center(child: const Text("My App")),
+          title: Center(child: const Text("My App")),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // To center we have to add SizeBox, Infinity.
+              SizedBox(
+                width: double.infinity,
+              ),
 
-            // To center we have to add SizeBox, Infinity.
-            SizedBox(
-              width: double.infinity,
-            ),
+              // Adding Image.
+              ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.asset(
+                  "assets/images/profile.png",
+                  width: 150,
+                  height: 150,
+                  fit: BoxFit.cover,
+                ),
+              ),
 
-            // Adding Image.
-            Image.asset(
-              "assets/images/profile.png",
-              width: 150,
-              height: 150,
-              fit: BoxFit.cover,
-            ),
+              // Spacing between Image and and Text.
+              SizedBox(height: 8),
 
-            // Spacing between Image and and Text.
-            SizedBox(height: 8),
+              // Normal Text.
+              Text("Nirmith Akash",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  )),
 
-            // Normal Text.
-            Text("Nirmith Akash",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              )
-            ),
-
-            // SizeBox to add space between elements.
-            SizedBox(height: 8),
-            // Styled Text.
-            Text(
-                "Title",
-                style: TextStyle(
+              // SizeBox to add space between elements.
+              SizedBox(height: 8),
+              // Styled Text.
+              Text("Title",
+                  style: TextStyle(
                     fontSize: 15,
                     fontStyle: FontStyle.italic,
                     color: Colors.teal,
-                )
-            ),
-          ],
-        ));
+                  )),
+              Row(
+                children: const <Widget>[
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.pink,
+                    size: 24.0,
+                    semanticLabel: 'Text to announce in accessibility modes',
+                  ),
+                  Icon(
+                    Icons.audiotrack,
+                    color: Colors.green,
+                    size: 30.0,
+                  ),
+                ],
+              ),
+            ]));
   }
 }
