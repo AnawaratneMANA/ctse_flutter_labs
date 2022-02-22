4fb1adcfb1018c1b56d4e5546cf232e745e7de46
AnawaratneMANA <it19086408@my.sliit.lk>
2022-02-22 15:06:22 +0530

    Content centered to the screen.

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({Key? key}): super(key: key);
+  const Profile({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("My App"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
+        appBar: AppBar(
+          title: const Text("My App"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
 
-          Text("Nirmith Akash"),
-          Text("Title", style: TextStyle(fontSize: 10, fontStyle: FontStyle.italic)),
-        ],
-      )
-    );
+            // To center we have to add SizeBox, Infinity.
+            SizedBox(
+              width: double.infinity,
+            ),
+
+            // Normal Text.
+            Text("Nirmith Akash"),
+
+            // Styled Text.
+            Text(
+                "Title",
+                style: TextStyle(
+                    fontSize: 10,
+                    fontStyle:
+                    FontStyle.italic)
+            ),
+          ],
+        ));
   }
 }
