c2e63af53288dc8da7292944f8b960b40c4bcf55
AnawaratneMANA <it19086408@my.sliit.lk>
2022-02-28 12:04:24 +0530

    Card component (ItemTile) created.

--- lib/components/item_tile.dart
+++ lib/components/item_tile.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../data.dart';
+
 class ItemTile extends StatelessWidget {
-  const ItemTile({Key? key}) : super(key: key);
+  final Item item;
+  const ItemTile({Key? key, required this.item}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Column(
-
+        children: [
+          Image.network(item.image),
+          Text(item.name, style: Theme.of(context).textTheme.headline6),
+          Text('${item.price}', style: Theme.of(context).textTheme.caption),
+        ],
       ),
     );
   }
